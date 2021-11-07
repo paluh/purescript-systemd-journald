d214b15907bd41b2e2e122f9fce5b2b7871912de
Tomasz Rybarczyk <paluho@gmail.com>
2021-11-07 11:05:34 +0100

    Tidy up

--- test/Main.purs
+++ test/Main.purs
@@ -2,25 +2,26 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
-
 import Node.Systemd.Journald (journald, debug)
 
-data Custom = Empty | NonEmpty Int
+data Custom
+  = Empty
+  | NonEmpty Int
 
 main :: Effect Unit
 main = do
-  logger ← journald { syslog_identifier: "purescript-systemd-journald-tests"  }
+  logger ← journald { syslog_identifier: "purescript-systemd-journald-tests" }
   let
     fields =
       { _ignored: "top"
       , something:
-        { how: "how"
-        , would: 888
-        , journald: \x → "some unicode: λ"
-        , store: "store"
-        , these: NonEmpty 8
-        , values: Empty
-        , _nonignored: "nested"
-        }
+          { how: "how"
+          , would: 888
+          , journald: \_ → "some unicode: λ"
+          , store: "store"
+          , these: NonEmpty 8
+          , values: Empty
+          , _nonignored: "nested"
+          }
       }
   debug logger "Test message" fields
